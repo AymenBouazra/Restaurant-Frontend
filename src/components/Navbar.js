e423e541430b9badf25ec9027518f64161788fb0
Aymen <aymenbouazra994@gmail.com>
2023-01-04 16:52:37 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Link className="navbar-brand" to="/">Offcanvas dark navbar</Link>
+          <Link to='/' className='nav-link text-light'> <FontAwesomeIcon icon={faShoppingCart} size='2xl' /></Link>
+          <div className="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+            <div className="offcanvas-header">
+              <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+              <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar

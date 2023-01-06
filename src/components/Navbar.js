f849c54810bd94043e8ad51e5849c7767e10bab7
Aymen <aymenbouazra994@gmail.com>
2023-01-06 02:17:14 +0100

    css update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,9 @@ const Navbar = () => {
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <Link className="navbar-brand" to="/">Offcanvas dark navbar</Link>
-          <Link to='/' className='nav-link text-light'> <FontAwesomeIcon icon={faShoppingCart} size='2xl' /></Link>
-          <div className="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <Link className="navbar-brand" to="/">E-commerce APP</Link>
+          <Link to='/' className='nav-link text-light'> <FontAwesomeIcon icon={faShoppingCart} size='xl' /></Link>
+          <div className="offcanvas offcanvas-start text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
             <div className="offcanvas-header">
               <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
               <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -21,7 +21,7 @@ const Navbar = () => {
             <div className="offcanvas-body">
               <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
                 <li className="nav-item">
-                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                  <Link className="nav-link" aria-current="page" to="/">Home</Link>
                 </li>
               </ul>
             </div>

70fb37d9b202fc581a6068571bad4886329d2e25
Aymen <aymenbouazra994@gmail.com>
2023-01-09 02:20:31 +0100

    commented home link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,9 +14,9 @@ const Navbar = ({openSideBar}) => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link className="nav-link" aria-current="page" to='/'>Home</Link>
-            </li>
+            </li> */}
           </ul>
           <Link to='/'><FontAwesomeIcon color='lightblue' icon={faShoppingCart} /></Link>
         </div>

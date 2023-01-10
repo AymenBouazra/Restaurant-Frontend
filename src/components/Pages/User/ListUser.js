6e8c0b03645fce2d90b7fd3b4d12171378432218
Aymen <aymenbouazra994@gmail.com>
2023-01-10 01:03:37 +0100

    list user done

--- src/components/Pages/User/ListUser.js
+++ src/components/Pages/User/ListUser.js
@@ -1,76 +1,12 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrash, faChevronUp} from '@fortawesome/free-solid-svg-icons'
+import { faChevronUp, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 import DataTable from 'react-data-table-component'
 
 // const ExpandedComponent = ({ data }) => <pre>{data.email}</pre>;
 
-const handleButtonClick = () => {
-  console.log('hello');
-}
-const columns = [
-  {
-    name: 'Username',
-    selector: row => row.userName,
-    sortable: true
-  },
-  {
-    name: 'E-mail',
-    selector: row => row.email,
-    sortable: true
-  },
-  {
-    name: 'Role',
-    selector: row => row.role,
-    sortable: true
-  },
-  {
-    width: '200px',
-    name: 'Actions',
-    cell: () => <div className='d-flex'>
-      <button onClick={handleButtonClick} className='btn btn-success me-2'><FontAwesomeIcon icon={faEdit}/></button>
-      <button onClick={handleButtonClick} className='btn btn-danger'><FontAwesomeIcon icon={faTrash}/></button>
-    </div>,
-    ignoreRowClick: true,
-    allowOverflow: true,
-    button: true,
-  },
-];
 
-const customStyles = {
-  table: {
-    style: {
-      border: '1px solid #d8e2dc'
-    }
-  },
-  tableWrapper: {
-    style: {
-      display: 'table'
-    }
-  },
-  rows: {
-    style: {
-      fontSize: "16px",
-
-    },
-    highlightOnHoverStyle: {
-      '&:nth-of-type(n)': {
-        backgroundColor: '#edf2fb',
-        transitionDuration: '0.5s',
-        transitionProperty: '#ddf0f8',
-        outlineStyle: 'solid',
-        outlineWidth: '0px',
-      }
-    },
-    
-  },
-  headCells: {
-    style: {
-      fontSize: '16px'
-    }
-  },
-}
 
 const ListUser = () => {
   const [users, setUsers] = useState([])
@@ -81,20 +17,94 @@ const ListUser = () => {
         <span className="visually-hidden">Loading...</span>
       </div>
     </div>
+  const columns = [
+    {
+      name: 'Username',
+      selector: row => row.userName,
+      sortable: true
+    },
+    {
+      name: 'E-mail',
+      selector: row => row.email,
+      sortable: true
+    },
+    {
+      name: 'Role',
+      selector: row => row.role,
+      sortable: true
+    },
+    {
+      width: '200px',
+      name: 'Actions',
+      selector: row => row._id,
+      cell: ({ _id }) => <div className='d-flex'>
+        <button onClick={() => handleUpdate(_id)} className='btn btn-success me-2'><FontAwesomeIcon icon={faEdit} /></button>
+        <button onClick={() => handleDelete(_id)} className='btn btn-danger'><FontAwesomeIcon icon={faTrash} /></button>
+      </div>,
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
+    },
+  ];
+
+  const customStyles = {
+    table: {
+      style: {
+        border: '1px solid #d8e2dc'
+      }
+    },
+    tableWrapper: {
+      style: {
+        display: 'table'
+      }
+    },
+    rows: {
+      style: {
+        fontSize: "16px",
+
+      },
+      highlightOnHoverStyle: {
+        '&:nth-of-type(n)': {
+          backgroundColor: '#edf2fb',
+          transitionDuration: '0.5s',
+          transitionProperty: '#ddf0f8',
+          outlineStyle: 'solid',
+          outlineWidth: '0px',
+        }
+      },
+    },
+    headCells: {
+      style: {
+        fontSize: '16px'
+      }
+    },
+  }
+
+  const handleDelete = async (id) => {
+    await axios.delete('http://localhost:4000/api/users/' + id)
+    getUsers()
+  }
+
+  const handleUpdate = async (id) => {
+    await axios.put('http://localhost:4000/api/users/' + id)
+    getUsers()
+  }
+
+  const getUsers = async () => {
+    const response = await axios.get('http://localhost:4000/api/users')
+    setUsers(response.data)
+    setPending(false)
+  }
+
   useEffect(() => {
     const controller = new AbortController();
-    const getUsers = async () => {
-      const response = await axios.get('http://localhost:4000/api/users')
-      setUsers(response.data)
-      setPending(false)
-    }
     getUsers()
     return controller.abort()
   }, [])
   return (
-    <div className='px-5'>
+    <div className='p-5'>
       <DataTable
-        title="List of users"
+        title={<h1 className='text-uppercase mb-5'>Users List</h1>}
         pagination
         columns={columns}
         data={users}
@@ -107,7 +117,8 @@ const ListUser = () => {
         highlightOnHover
         pointerOnHover
         fixedHeaderScrollHeight="700px"
-        sortIcon={<FontAwesomeIcon color='lightblue' size='xs' icon={faChevronUp} />}
+        sortIcon={<FontAwesomeIcon className='ms-2' color='lightblue' icon={faChevronUp} />}
+        getUsersFn={getUsers}
       />
     </div>
   )

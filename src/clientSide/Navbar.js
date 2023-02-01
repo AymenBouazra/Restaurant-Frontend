3fe19c177c5ad8dd5ac872647d9b5921f6b728c8
Aymen <aymenbouazra994@gmail.com>
2023-02-01 23:11:30 +0100

    updated navbar

--- src/clientSide/Navbar.js
+++ src/clientSide/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-nav">
       <div className="container">
-        <Link className="navbar-brand" to="/">Navbar</Link>
+        <Link className="navbar-brand" to="/">Restaurant</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -15,7 +15,7 @@ const Navbar = () => {
               <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/food">Link</Link>
+              <Link className="nav-link" to="/food">Food</Link>
             </li>
           </ul>
         </div>
